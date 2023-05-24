d7e8641c6a6aa0c70d2ce8ebba21624bbab50c06
hellbovh <69521565+hellbovh@users.noreply.github.com>
2023-05-24 23:44:28 +0300

    B28 petrov

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,24 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> tree;
+  std::string word;
+  std::ifstream fin;
+  fin.open(filename);
+  if (!fin.is_open()) {
+    throw std::string("file opening error!");
+  }
+  while (file >> word) {
+    std::string formatted_word = "";
+    for (int i = 0; i < word.length(); i++) {
+      if (isalpha(word[i])) {
+        formatted_word += tolower(word[i]);
+      }
+    }
+    if (formatted_word != "") {
+      tree.add(formatted_word);
+    }
+  }
+  fin.close();
+  return tree;
 }
